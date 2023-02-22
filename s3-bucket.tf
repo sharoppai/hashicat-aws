02384623ac7aa8bc06efca90a25f99b81c72d719
Indra Hioe <indra@cnavbv.com>
2023-02-22 04:45:11 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,8 +2,6 @@ module "s3_bucket" {
   source  = "app.terraform.io/CNAVBV/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
-  acl    = "private"
   bucket_prefix = "${var.prefix}"
 
   versioning = {
